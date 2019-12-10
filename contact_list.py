73f63572a5a538e1eb4ba1d0535d8f96fc4c44be
Brandon Thompson <thompsonbrandon13@yahoo.com>
2019-12-10 00:49:09 -0600

    init and str functions

--- contact_list.py
+++ contact_list.py
@@ -2,10 +2,14 @@
 # Contact List for Software Engineering IV Final
 
 class Person():
-    def __init__(name, number, email):
-        self.name = name
+    def __init__(self, first_name, last_name, number, email):
+        self.first_name = first_name
+        self.last_name = last_name
         self.number = number
         self.email = email
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 
 def display_menu():
@@ -20,3 +24,10 @@ def display_menu():
 
 if __name__ == "__main__":
     display_menu()
+
+    bt = Person("Brandon", "Thompson", "2146688475", "bthomp24@gmail.com")
+    myDict = {
+        bt.last_name : bt,
+    }
+
+    print(myDict.get(bt.last_name))
